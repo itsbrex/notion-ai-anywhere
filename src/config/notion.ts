e2d0ae378fc87187b21fb52c41f4f7978a412313
itsbrex <roach7@gmail.com>
2023-05-30 13:11:27 -0700

    Initialize Notion SDK and fix typo in secretKeysList in src/config/notion.ts

--- src/config/notion.ts
+++ src/config/notion.ts
@@ -1,12 +1,12 @@
 // initialize notion sdk
+// initialize notion sdk
 import { Client } from "@notionhq/client"
-
 import { Storage } from "@plasmohq/storage"
 
 const getNotion = async () => {
   const storage = new Storage({
     area: "session",
-    secretKeyList: ["token"]
+    // secretKeysList: ["token"]
   })
   const token = await storage.get("token")
   const notion = new Client({
